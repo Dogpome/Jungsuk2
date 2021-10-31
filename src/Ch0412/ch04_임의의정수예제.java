3713c3e0bb32dd33f3e85d6a806e57fb6b457057
Kyeongrok Kim <oceanfog1@gmail.com>
2021-10-31 15:25:22 +0900

    주석 추가

--- src/Ch0412/ch04_임의의정수예제.java
+++ src/Ch0412/ch04_임의의정수예제.java
@@ -7,7 +7,7 @@ public class ch04_임의의정수예제 {
         for (int i = 1; i  <= 20; i++){
             // 포문은 반복문 괄호 안 내용을 20번 반복한다는 뜻.
             System.out.println(Math.random());
-            //https://www.youtube.com/watch?v=OPXM_2lagLA&list=PLW2UjW795-f6xWA2_MUhEVgPauhGl3xIp&index=35
+            // 동영상 강의 link random : https://www.youtube.com/watch?v=OPXM_2lagLA&list=PLW2UjW795-f6xWA2_MUhEVgPauhGl3xIp&index=35
         }
     }
 }
