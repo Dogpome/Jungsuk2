155cc0c7c6dddf314c1e9becf92ff654efcfbe7d
DESKTOP-GLUVOJU\mirro <mirrorkyh@gmail.com>
2021-10-31 16:28:24 +0900

    구구단 출력, i 를 인식하지 못함.

---
+++ src/Ch0412/ch04_구구단.java
@@ -0,0 +1,10 @@
+package Ch0412;
+
+public class ch04_구구단 {
+    public static void main(String[] args) {
+        for(int i = 2; i <= 10; i++); {
+            System.out.println(i);
+
+        }
+    }
+}
