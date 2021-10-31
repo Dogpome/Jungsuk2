fb9976cd7a391cd31e0c570a2def4af8303f2cb5
DESKTOP-GLUVOJU\mirro <mirrorkyh@gmail.com>
2021-10-31 16:46:26 +0900

    중첩 포문까지 구구단 출력임.

--- src/Ch0412/ch04_구구단.java
+++ src/Ch0412/ch04_구구단.java
@@ -2,8 +2,11 @@ package Ch0412;
 
 public class ch04_구구단 {
     public static void main(String[] args) {
-        for(int i = 2; i <= 10; i++); {
-            System.out.println(i);
+        for(int i = 2; i <= 9; i++) {
+            System.out.println(i+"단을 출력합니다");
+            for (int x = 1; x <= 9; x++) {
+                System.out.println(i+"x"+i+"="+i*x); // 중첩포문까지 한거임.
+            }
 
         }
     }
