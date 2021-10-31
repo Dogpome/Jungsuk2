89a17e0e01c4533582bb12e3b40387adc779c4f3
DESKTOP-GLUVOJU\mirro <mirrorkyh@gmail.com>
2021-10-31 15:42:56 +0900

    0~9사이의 정수 50개 출력 Math.random()사용

---
+++ src/Ch0412/ch04_임의의정수0에서9.java
@@ -0,0 +1,14 @@
+package Ch0412;
+
+public class ch04_임의의정수0에서9 {
+    public static void main(String[] args) {
+        int num = 0;
+        // 0~9사이의 정수 50개를 출력 하시오
+        for (int i = 1; i  <= 50; i++){
+            num = (int) (Math.random()*10);
+            // 포문은 반복문 괄호 안 내용을 20번 반복한다는 뜻.
+            System.out.println(num);
+            // 동영상 강의 link random : https://www.youtube.com/watch?v=OPXM_2lagLA&list=PLW2UjW795-f6xWA2_MUhEVgPauhGl3xIp&index=35
+        }
+    }
+}
