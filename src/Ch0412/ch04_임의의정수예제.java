f3328e0469cb71ead8236e3da1c5107c078110c0
DESKTOP-GLUVOJU\mirro <mirrorkyh@gmail.com>
2021-10-31 15:23:35 +0900

    동영상 강의 주석 추가.

--- src/Ch0412/ch04_임의의정수예제.java
+++ src/Ch0412/ch04_임의의정수예제.java
@@ -3,11 +3,11 @@ package Ch0412;
 public class ch04_임의의정수예제 {
     public static void main(String[] args) {
         int num = 0;
-        System.out.println("Hello");
         // 괄호{} 안의 내용을 20번 반복한다.
-        for (int i = 1; i <= 20; i++){
+        for (int i = 1; i  <= 20; i++){
             // 포문은 반복문 괄호 안 내용을 20번 반복한다는 뜻.
             System.out.println(Math.random());
+            //https://www.youtube.com/watch?v=OPXM_2lagLA&list=PLW2UjW795-f6xWA2_MUhEVgPauhGl3xIp&index=35
         }
     }
 }
